ab47f639436aad2fdb5eaf9f762350719dbde502
PoJIiTuK <53650630+PoJIiTuK@users.noreply.github.com>
2020-11-21 15:04:36 +0200

    Update BagsTrackerTest.py

--- BagsTrackerTest.py
+++ BagsTrackerTest.py
@@ -1,20 +1,16 @@
 import unittest
-from BagsTracker import get_min_size_of_square
+from BagsTracker import find_min_size
 
 
 class BagsTracker(unittest.TestCase):
-    def test_one_element(self):
-        self.assertEqual(get_min_size_of_square(1, 1, 100), 100)
-        self.assertEqual(get_min_size_of_square(1, 100, 1), 100)
+    def test_1(self):
+        self.assertEqual(find_min_size(1, 1, 100), 100)
 
-    def test_big_numbers(self):
-        self.assertEqual(get_min_size_of_square(10, 100000000, 99999999), 399999996)
+    def test_2(self):
+        self.assertEqual(find_min_size(14, 3459809345, 34534589), 3459809345)
 
-    def test_width_is_one(self):
-        self.assertEqual(get_min_size_of_square(10, 1, 10), 10)
+    def test_3(self):
+        self.assertEqual(find_min_size(10, 10, 1), 10)
 
-    def test_height_is_one(self):
-        self.assertEqual(get_min_size_of_square(10, 10, 1), 10)
-
-    def test_from_condition(self):
-        self.assertEqual(get_min_size_of_square(10, 2, 3), 9)
+    def test_4(self):
+        self.assertEqual(find_min_size(10, 2, 3), 9)
